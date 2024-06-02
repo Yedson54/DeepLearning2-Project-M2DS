fix: update cross entropy and batch loss computation

src/models/dnn.py
@@ -136,9 +136,9 @@ class DNN(DBN):
             (dZ_lead @ dW_lead.T)
             * layer_outputs[id_layer]
             * (1 - layer_outputs[id_layer])
-        )
+        ) # / batch_size
         dW = layer_outputs[id_layer - 1].T @ dZ
-        db = np.sum(dZ, axis=0, keepdims=True)
+        db = np.sum(dZ, axis=0)
 
 
         # Update hidden layer weights and biases (layer no. `id_layer` + 1).
@@ -180,26 +180,27 @@ class DNN(DBN):
             shuffled_data = input_data[shuffled_indices]
             shuffled_labels = labels[shuffled_indices]
 
+            loss = 0.0
             for batch_start in range(0, n_samples, batch_size):
                 # Shuffle data and labels.
                 batch_end = min(batch_start + batch_size, n_samples)
                 batch_input = shuffled_data[batch_start:batch_end]
                 batch_labels = shuffled_labels[batch_start:batch_end]
 
-                # Forward pass
+                # Forward pass.
                 layer_outputs = self.input_output_network(batch_input)
 
                 # Backward pass (update weights and biases)
                 ## Compute output (last) layer gradients (layer L).
                 dZ = (layer_outputs[-1] - batch_labels) # -> (n_samples, output_dim) -> (self[-2].n_hidden, self[-1].n_hidden)
                 dW = layer_outputs[-2].T @ dZ # -> (1, self[-1].n_hidden)
-                db = np.sum(dZ, axis=0, keepdims=True)
+                db = np.sum(dZ, axis=0)
                 ## Update output (last) layer parameters (layer L).
                 self.network[-1].W -= learning_rate * dW
                 self.network[-1].b -= learning_rate * db
                 self.n_iter += 1
 
-                ## Iterate layer in reverse order
+                ## Iterate layer in reverse order.
                 for id_layer in range(-2, -len(self.network)):
                     dZ, dW = self.update(
                         dZ_lead=dZ,
@@ -210,11 +211,11 @@ class DNN(DBN):
                         learning_rate=learning_rate,
                     )
 
+                # Update cross entropy.
+                loss += F.cross_entropy(batch_labels, layer_outputs[-1], eps)
+                 
             # HACK: update discrepancy / ensure consistency between self.rbms and self.network[:-1]
             self.rbms = self.network[:-1]
-
-            # Calculate cross entropy after each epoch
-            loss = F.cross_entropy(batch_labels, layer_outputs[-1], eps)
             tqdm.write(f"Epoch {epoch + 1}/{n_epochs}, Cross Entropy: {loss}")
             losses.append(loss)
 
@@ -233,10 +234,10 @@ class DNN(DBN):
         - float: Classification error rate.
         """
         # Estimate labels using the trained DNN
-        estimated_labels = self.input_output_network(test_data)[-1]
+        softmax_labels = self.input_output_network(test_data)[-1]
 
         # Convert softmax probabilities to one-hot encoded predictions
-        estimated_labels_one_hot = get_predictions_one_hot(estimated_labels)
+        estimated_labels_one_hot = get_predictions_one_hot(softmax_labels)
 
         # Calculate classification error rate
         error_rate = classification_error_rate(estimated_labels_one_hot, true_labels)

src/utils.py
@@ -12,16 +12,16 @@ DATA_FOLDER = "../data/"
 ALPHA_DIGIT_PATH = os.path.join(DATA_FOLDER, "binaryalphadigs.mat")
 
 
-###########################################
-################ DATA: ETL ################
-###########################################
+##############################################################################
+######################## DATA: Extract, Transform, Load ######################
+##############################################################################
 def load_alphadigit(alphadigit_path):
     """Load the binary AlphaDigits dataset from a .mat file."""
     return scipy.io.loadmat(alphadigit_path)["dat"]
 
-
 def load_mnist(
-    mnist_path: str, dataset_type: Literal["train", "test", "all"] = "all"
+    mnist_path: str, 
+    dataset_type: Literal["train", "test", "all"] = "all"
 ) -> Union[
     Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]
 ]:
@@ -184,9 +184,63 @@ def get_predictions_one_hot(y_pred_probas: np.ndarray) -> np.ndarray:
     return predictions_one_hot
 
 
-######################################
-################ PLOT ################
-######################################
+def sample_indices(data_size: int, sample_size: int, random_state=None) -> np.ndarray:
+    """
+    Randomly sample indices from the range [0, data_size) without replacement.
+
+    Parameters:
+        data_size (int): Total number of indices available.
+        sample_size (int): Number of indices to sample.
+        random_state: Random seed for reproducibility.
+
+    Returns:
+        numpy.ndarray: Array of sampled indices.
+    """
+    rng = np.random.default_rng(random_state)
+    if sample_size >= data_size:
+        return np.arange(data_size)
+
+    sampled_indices = rng.choice(data_size, size=sample_size, replace=False)
+    return sampled_indices
+
+
+def sample_mnist(
+    X_train: np.ndarray,
+    y_train: np.ndarray,
+    sample_size: int,
+    random_state=None
+) -> tuple:
+    """
+    Randomly sample a subset of samples from the MNIST dataset.
+
+    Parameters:
+        X_train (numpy.ndarray): Training data.
+        y_train (numpy.ndarray): Training labels.
+        sample_size (int): Number of samples to sample.
+        random_state: Random seed for reproducibility.
+
+    Returns:
+        tuple: Sampled subset of training data and labels.
+    """
+    # Sample indices
+    indices = sample_indices(len(X_train), sample_size, random_state=random_state)
+
+    # Sample data and labels
+    sampled_X_train = X_train[indices]
+    sampled_y_train = y_train[indices]
+
+    return sampled_X_train, sampled_y_train
+
+
+def binarize_images(images):
+    threshold = 127
+    binary_images = np.where(images > threshold, 1, 0)
+    return binary_images
+
+
+##############################################################################
+############################ PLOT FUNCTION ###################################
+##############################################################################
 def plot_characters_alphadigit(
     chars: List[Union[str, int]],
     data: np.ndarray,