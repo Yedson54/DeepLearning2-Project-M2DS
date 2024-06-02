62937f483162e86fe31236ee9623dd2f9f316104
Yedidia AGNIMO <ayedidyas@gmail.com>
2024-06-02 16:04:41 +0200

    feat: start new rbm experiment

--- src/models/dbn.py
+++ src/models/dbn.py
@@ -1,12 +1,13 @@
 """
 Module: principal_dbn_alpha.py
-Module providing implementation of Deep Belief Network (DBN).
+Module providing implementation of Deep Belief Network (DBN). 
 """
 
 from typing import List
 
 import numpy as np
 from tqdm import tqdm
+from IPython.display import clear_output
 
 from models.rbm import RBM
 
@@ -40,7 +41,7 @@ class DBN:
 
         # Initialize the first RBM
         first_rbm = RBM(
-            n_visible=n_visible,
+            n_visible=self.n_visible,
             n_hidden=hidden_layer_sizes[0],
             random_state=random_state,
         )
@@ -48,12 +49,13 @@ class DBN:
 
         # Initialize RBMs for subsequent hidden layers
         for i, size in enumerate(hidden_layer_sizes[1:], start=1):
-            rbm = RBM(
-                n_visible=hidden_layer_sizes[i - 1],
-                n_hidden=size,
-                random_state=random_state,
+            self.rbms.append(
+                RBM(
+                    n_visible=hidden_layer_sizes[i - 1],
+                    n_hidden=size,
+                    random_state=random_state,
+                )
             )
-            self.rbms.append(rbm)
 
     def __getitem__(self, key):
         return self.rbms[key]
@@ -74,6 +76,7 @@ class DBN:
         batch_size: int = 10,
         print_each: int = 10,
         verbose=False,
+        **kwargs,
     ) -> "DBN":
         """
         Train the DBN using Greedy layer-wise procedure.
@@ -84,26 +87,36 @@ class DBN:
         - n_epochs (int): Number of training epochs. Default is 10.
         - batch_size (int): Size of mini-batches. Default is 10.
         - print_each: Print reconstruction error each `print_each` epochs.
+        - verbose (bool): Whether to print training progress. Default is False.
+        - **kwargs: Additional arguments to be passed to RBM.train().
 
         Returns:
         - DBN: Trained DBN instance.
         """
-        input_data = data
+        input_data = data.copy()
+
+        errors = []
+        losses = []
         for rbm in tqdm(self.rbms, desc="Training RBM layers", unit="layer"):
-            rbm.train(
+            errors_rbm = rbm.train(
                 input_data,
                 learning_rate=learning_rate,
                 n_epochs=n_epochs,
                 batch_size=batch_size,
                 print_each=print_each,
-                verbose=verbose
+                verbose=verbose,
+                **kwargs,
             )
+            errors.extend(errors_rbm)
+            losses.extend(errors_rbm[-1])
             # Update input data for the next RBM
-            input_data = rbm.input_output(input_data)
+            h_probs, input_data = rbm.input_output(input_data)
 
-        return self
+        return errors, losses
 
-    def generate_image(self, n_samples: int = 1, n_gibbs_steps: int = 1) -> np.ndarray:
+    def generate_image(
+        self, n_samples: int = 1, n_gibbs_steps: int = 100
+    ) -> np.ndarray:
         """
         Generate samples from the DBN using Gibbs sampling.
 
@@ -118,9 +131,9 @@ class DBN:
 
         # Generate samples using the first RBM in the DBN
         samples = self.rbms[-1].generate_image(n_samples, n_gibbs_steps)
+
         for rbm in reversed(self.rbms[:-1]):
             # Sample from the conditional probability of layer l-1 given layer l: p(h_{s-1}|h_{s}).
-            h_probs = rbm.output_input(samples)
-            h = self.rng.binomial(1, p=h_probs)
-            samples = h
+            h_probs, samples = rbm.output_input(samples)
+            
         return samples
